baabb5adaf0f7eb41a631cce232ebe514872cf27
wenderson dhomini <wendersondhomini125@gmail.com>
2024-05-04 15:52:23 -0300

    Iniciando projeto

---
+++ contaBanco/src/ContaTerminal.java
@@ -0,0 +1,9 @@
+package dioTrilhaJavaBasico.contaBanco.src;
+
+public class ContaTerminal {
+    public static void main(String[] args) {
+
+        System.out.print("Hello and welcome!");
+
+    }
+}
