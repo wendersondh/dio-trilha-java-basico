356a4b01a460b308e76250c0e87f9ea7a5183e6d
wenderson dhomini <wendersondhomini125@gmail.com>
2024-05-04 16:34:09 -0300

    implementando o código

--- contaBanco/src/ContaTerminal.java
+++ contaBanco/src/ContaTerminal.java
@@ -1,9 +1,30 @@
 package dioTrilhaJavaBasico.contaBanco.src;
 
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
-        System.out.print("Hello and welcome!");
+        System.out.println("Olá, seja bem vindo");
+        System.out.println("Por favor, digite o número da sua conta: ");
+        int numero = scanner.nextInt();
+
+        System.out.println("Agora, digite o número da Agência");
+        String agencia = scanner.next();
+
+        System.out.println("Nome do cliente: ");
+        String nome = scanner.next();
+
+        System.out.println("Saldo da conta: ");
+        double saldo = scanner.nextDouble();
+
+        System.out.println("Olá "+ nome +
+                ", obrigado por criar uma conta em nosso banco, sua agência é "+ agencia +
+                ", conta "+ numero +
+                " e seu saldo "+ saldo +
+                " já está disponível para saque");
+
 
     }
 }
